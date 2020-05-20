b6a62fc90ddd1df257171f57b23a984fc853aa57
YutaNakamura <gp17a089@oecu.jp>
2020-05-21 02:05:41 +0900

    add weight and bias

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
+import numpy as np
+
+
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1*w1 + x2*w2
-    if tmp <= theta:
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
         return 0
-    elif tmp > theta:
-        return 1
-def OR(x1,x2):
-    w1 ,w2, theta = 0.7, 0.7, 0.5
-    tmp = x1*w1 + x2*w2
-    if tmp <= theta:
-        return 0
-    elif tmp > theta:
+    else:
         return 1
 
+def OR(x1,x2):
+    x = np.array([x1, x2])
+    w = np.array([0.7, 0.7,])
+    b = -0.5
+    tmp = np.sum(x*w) +b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+    
+def tbl(logic):
+    x1 = [0, 1]
+    x2 = [0, 1]
+    for a in x1:
+        for b in x2:
+            if logic == AND:
+                print(f'{a} AND {b} = ' + '{}'.format(logic(a, b)))
+            elif logic == OR:
+                print(f'{a} O R {b} = ' + '{}'.format(logic(a,b)))
+            elif logic == NAND:
+                print(f'{a} NAND {b} = ' + '{}'.format(logic(a,b)))
+
 def main():
-    a=AND(0, 1)
-    b=OR(1, 1)
-    c=OR(0, 0)
-    print(a)
-    print(b)
-    print(c)
+    tbl(AND)
+    tbl(OR)
+    tbl(NAND)
 
 if __name__ == "__main__":
     main()
